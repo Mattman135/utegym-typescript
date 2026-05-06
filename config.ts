a7565818113de8a339d4e4b50948757e8b9938bd
Mattis Hallberg <skatemattis97@hotmail.com>
2026-05-06 20:35:05 +0200

    bug fix

--- config.ts
+++ config.ts
@@ -7,7 +7,7 @@ const config = {
   appDescription:
     "Utegym webbkatalog",
   // REQUIRED (no https://, not trialing slash at the end, just the naked domain)
-  domainName: "utegymnäramig.se",
+  domainName: "xn--utegymnramig-mcb.se",
   crisp: {
     // Crisp website ID. IF YOU DON'T USE CRISP: just remove this => Then add a support email in this config file (resend.supportEmail) otherwise customer support won't work.
     id: "",
